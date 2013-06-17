8f8cd21dd500ca937a6fd3dc14d8a90786abff6e
yueyoum <yueyoum@gmail.com>
2013-06-17 21:48:07 +0800

    强制用户设置SITEUSER_ACCOUNT_MIXIN,否则报错

--- siteuser/utils/load_user_define.py
+++ siteuser/utils/load_user_define.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 
 class UserNotDefined(object):pass
 
 def user_defined_mixin():
     mixin = getattr(settings, 'SITEUSER_ACCOUNT_MIXIN', UserNotDefined)
+    if mixin is UserNotDefined:
+        raise ImproperlyConfigured("No Settings For SITEUSER_ACCOUNT_MIXIN")
     if mixin is object:
-        raise AttributeError("Invalid SITEUSER_ACCOUNT_MIXIN")
+        raise ImproperlyConfigured("Invalid SITEUSER_ACCOUNT_MIXIN")
     if isinstance(mixin, type):
         return mixin
 
